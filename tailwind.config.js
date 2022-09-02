7671daec40c67b6c38d9c0e9064464532c6c067a
piggydoughnut <piggydoughnut@gmail.com>
2022-09-02 13:12:59 +0800

    added cv

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
-/** @type {import('tailwindcss').Config} */ 
-const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["index.html"],
   theme: {
     fontSize: {
-      'tiny': '12px',
-      'base': '16px',
-      'sm': '18px',
-      'lg': '24px',
-      'xl': '36px'
+      tiny: "12px",
+      small: "14px",
+      base: "16px",
+      sm: "18px",
+      lg: "24px",
+      xl: "36px",
     },
     extend: {
       fontFamily: {
-        firaSans: ['Fira Sans', 'sans-serif']
+        firaSans: ["Fira Sans", "sans-serif"],
       },
       colors: {
-        'purple-link': '#5551FF', // light purple
-        'midnight-black': '#282828', // midnight-black
-      }
+        "purple-link": "#5551FF", // light purple
+        "midnight-black": "#282828", // midnight-black
+      },
     },
   },
   plugins: [],
-}
+};
